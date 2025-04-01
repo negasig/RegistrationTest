2d21bd081f7e6b89b3c778e3b310e87d9127b674
Biniam Belay <nigertsewar1984@gmail.com>
2025-04-01 16:05:39 +0300

    edit config file

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
  
-/* Attempt to connect to MySQL database */
+/* Attempt to connect to MySQL database kgik*/
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
